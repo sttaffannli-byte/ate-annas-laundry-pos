24b41f18e6ddcda6bf4c7763b18883f9783e7641
sttaffannli-byte <sttaffannli@gmail.com>
2026-07-21 15:56:04 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="ate-annas-laundry-pos-v42-realtime";
+const CACHE="ate-annas-laundry-pos-v421-realtime-fix";
 const ASSETS=["/","/index.html","/styles.css","/app.js","/booking.html","/booking.css","/booking.js","/manifest.webmanifest","/_routes.json"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))));self.clients.claim()});
